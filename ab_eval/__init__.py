fc0c5b3f1875e3c6e3263aee3dfadf2751e3b444
yiannis.moschonas <yiannis.moschonas@deliveryhero.com>
2018-10-12 18:21:58 +0200

    init

---
+++ ab_eval/__init__.py
@@ -0,0 +1,10 @@
+"""Main ExpAn module that contains core and data modules."""
+
+from __future__ import absolute_import
+
+import logging.config
+
+from ab_eval.core import *
+
+__all__ = ["core"]
+logging.basicConfig(level=logging.DEBUG)
