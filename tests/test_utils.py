3f9033534e27695551851e61eed0deb116fcc044
yiannis.moschonas <yiannis.moschonas@deliveryhero.com>
2018-10-17 11:19:17 +0200

    add some tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,18 @@
 from ab_eval.core.utils import *
 
 
+
+
 def test_get_segments_sample_size():
     df=generate_random_cvr_data(1000,0.3,0.5)
     assert get_segments_sample_size(df) == 1000
+
+def test_get_test_summary_without_segment():
+    df=generate_random_cvr_data(1000,0.3,0.5,days=10,control_label='control',variation_label='variation')
+    df1=get_test_summary(df,'CVR')
+    assert df1['CVR'].control != None
+
+def test_get_test_summary_with_segment():
+    df=generate_random_cvr_data(1000,0.3,0.5,days=10,control_label='control',variation_label='variation')
+    df1=get_test_summary(df,'CVR',segment='new')
+    assert df1['CVR'].control != None
