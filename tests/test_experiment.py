40935b23a028a3f7b44436f0a345919f5356498a
yiannis.moschonas <yiannis.moschonas@deliveryhero.com>
2018-10-18 10:38:22 +0200

    add test for variation class

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,4 +1,4 @@
-from ab_eval.core.experiment import evaluation_metrics
+from ab_eval.core.experiment import evaluation_metrics , variations
 
 
 
@@ -6,3 +6,7 @@ from ab_eval.core.experiment import evaluation_metrics
 def test_primary_kpi_existance():
     metrics=evaluation_metrics('mCVR1')
     assert metrics.primary_KPI in metrics.kpis
+
+def test_variations():
+    variation=variations(variation_label='control')
+    assert variation.variation_label == 'control'
