4182d1d2caf531a626fa9387aa495d281a545570
yiannis.moschonas <yiannis.moschonas@deliveryhero.com>
2018-10-29 10:15:50 +0100

    styling

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,17 +1,16 @@
-from ab_eval.core.experiment_components import evaluation_metrics , variations
-
+from ab_eval.core.experiment_components import evaluation_metrics, variations
 from ab_eval.core.experiment import experiment
 from ab_eval.core.utils import generate_random_cvr_data
 
 
 def test_primary_kpi_existance():
-    metrics=evaluation_metrics('mCVR1')
+    metrics = evaluation_metrics('mCVR1')
     assert metrics.primary_KPI in metrics.kpis
 
-def test_variations():
-    variation=variations(variation_label='control')
-    assert variation.variation_label == 'control'
 
+def test_variations():
+    variation = variations(variation_label='control')
+    assert variation.variation_label == 'control'
 
 
 def test_get_p_value_on_random_data():
