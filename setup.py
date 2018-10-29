6beecb65680b7602bc6aa81b46f67a6b98b4255f
yiannis.moschonas <yiannis.moschonas@deliveryhero.com>
2018-10-29 10:17:18 +0100

    styling

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-import re
 from setuptools import setup, find_packages
+
 
 def parse_requirements(filename):
     """ load requirements from a pip requirements file """
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
 
+
 try:
     requirements = parse_requirements('requirements.txt')
 except OSError:
-	requirements = []
+    requirements = []
 
 setup(
     name='ab_eval',
@@ -24,6 +25,6 @@ setup(
     license="MIT",
     zip_safe=False,
     keywords='ab_eval',
-    #test_suite='tests',
-    #tests_require=test_requirements
+    # test_suite='tests',
+    # tests_require=test_requirements
 )
