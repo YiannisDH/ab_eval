116f8ea47d00d90d220217303f4328fee6ce2431
yiannis.moschonas <yiannis.moschonas@deliveryhero.com>
2018-10-31 16:47:57 +0100

    fix p value and z score, and add alternative attribute to the experiment class

--- ab_eval/core/experiment.py
+++ ab_eval/core/experiment.py
@@ -2,7 +2,7 @@ import json
 import logging
 from ab_eval.core.experiment_components import variations, evaluation_metrics
 from ab_eval.core.utils import get_test_summary, get_standard_error, get_z_val
-import scipy.stats as scs
+import statsmodels.api as sm
 import numpy as np
 
 logger = logging.getLogger(__name__)
@@ -28,6 +28,7 @@ class experiment(object):
             kpis=evaluation_metrics(kpis=["CVR"]),
             variations=variations(),
             segments=None,
+            alternative='two-sided',
             significance_level=0.05,
             *args, **kwargs):
         super(experiment, self).__init__(*args, **kwargs)
@@ -35,6 +36,7 @@ class experiment(object):
         self.kpis = kpis
         self.variations = variations
         self.segments = segments
+        self.alternative = alternative
         if significance_level > 1:
             raise ValueError("significance_level should be >0 and <1 : {}")
         self.significance_level = significance_level
@@ -73,9 +75,12 @@ class experiment(object):
 
         df_summary = get_test_summary(self.data, kpi=kpi, segment=segment, segment_column=segment_column, variations_column=variation_column)
 
-        return scs.binom(df_summary['total'][self.variations.variation_label],
-                         df_summary['rate'][self.variations.variation_label])\
-            .pmf(df_summary['rate'][self.variations.control_label] * df_summary['total'][self.variations.control_label])
+        zscore, pval = sm.stats.proportions_ztest([df_summary[kpi][self.variations.variation_label], df_summary[kpi][self.variations.control_label]],
+                                                  [df_summary['total'][self.variations.variation_label],
+                                                  df_summary['total'][self.variations.control_label]],
+                                                  alternative=self.alternative)
+
+        return {"z-score": zscore, 'p-value': pval}
 
     def get_relative_conversion_uplift(self, kpi='CVR', segment=None, segment_column='segment', variation_column='group'):
         """Method that calculates the relative conversion_uplift
@@ -137,7 +142,7 @@ class experiment(object):
 
         M1 = df_summary['rate'][self.variations.variation_label]
         M2 = df_summary['rate'][self.variations.control_label]
-        z = get_z_val(sig_level=self.significance_level, two_tailed=True)
+        z = get_z_val(sig_level=self.significance_level, two_tailed=True if self.alternative == 'two-sided' else False)
         std1, std2 = self.get_standard_errors_of_test(kpi=kpi, segment=segment, segment_column=segment_column, variation_column=variation_column)
         std1 *= std1
         std2 *= std2
