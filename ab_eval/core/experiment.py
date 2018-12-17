64c5909041172004f0145d8103367e4eab415cb3
Yiannis Moschonas <yiannis.moschonas@deliveryhero.com>
2018-12-17 13:55:10 +0100

    change calculation of get_confidence_interval_of_test

--- ab_eval/core/experiment.py
+++ ab_eval/core/experiment.py
@@ -255,7 +255,7 @@ class experiment(object):
         std2 = get_standard_deviation(M2)
         std1 *= std1
         std2 *= std2
-        Sm1_m2 = (np.sqrt((std1 + std2 / (2 / (1 / M1 + 1 / M2)))))
+        Sm1_m2 = (np.sqrt(((N1 - 1) * std1 ^ 2 + (N2-1) * std2 ^ 2) / (N1 + N2 - 2) ) )
         SE1_2 = Sm1_m2(np.sqrt(1 / N1 + 1 / N2))
 
         return {"lower_limit": M1 - M2 - z * SE1_2, "upper_limit": M1 - M2 + z * SE1_2}
